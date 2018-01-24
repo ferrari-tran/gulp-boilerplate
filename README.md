a7ae335ce775d68a1f73d57cea9e6fad04551ff8
Lao Ton <laoton@Laos-MacBook-Pro.local>
2018-01-24 09:54:25 +0700

    done inject

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Gulp Boilerplate for Landing page
-======
+
 
 ### 1. How to on/off browser sync?
 Stop scroll, click, form action in all devices
@@ -8,3 +8,7 @@ Stop scroll, click, form action in all devices
 http://localhost:8080/sync-options
 ```
 Choose options what you want to control.
+
+
+### 2. Syntax EJS
+[Syntax as markdown](https://github.com/mde/ejs/blob/master/docs/syntax.md)
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,67 @@
 var gulp 						= require('gulp'),
-	argv 						= require('yargs').argv, // watching when start gulp
-	browserSync 				= require('browser-sync'); // sync all browser when run gulp
+	ejs 						= require('gulp-ejs'), 						// language ejs
+	log 						= require('fancy-log'), 					// log error for ejs
+	argv 						= require('yargs').argv, 					// watching when start gulp
+	inject 						= require('gulp-inject'), 					// auto add file vendor to page
+	config 						= require('./build.config.json'), 			// link file vendors
+	browserSync 				= require('browser-sync'), 					// sync all browser when run gulp
+	reload 						= browserSync.reload; 						// reload page if change
 	
 var path = {
 	source: './src',
-	output: './dist'
+	output: './dist',
+	vendors: './src/assets/vendors',
+	module: './node_modules'
 };
 
-gulp.task('html', () => {
-	return gulp.src(path.source + '/*.html');
+
+/**
+ * ===============================================================
+ * Task: EJS
+ * ===============================================================
+ */
+gulp.task('ejs', () => {
+	return gulp.src(path.source + '/pages/*.ejs')
+		.pipe(ejs({},{}, {ext: '.html'}).on('error', log))
+	    .pipe(gulp.dest(path.output))
+	    .pipe(reload({stream: true}));
 });
 
-// Tast: Watching task when start gulp
+
+/**
+ * ===============================================================
+ * Task: COPY
+ * ===============================================================
+ */
+gulp.task('copy', () => {
+	return gulp.src(config.src)
+		.pipe(gulp.dest(path.vendors))
+		.pipe(reload({stream: true}));;
+});
+
+
+
+/**
+ * ===============================================================
+ * Task: INJECT - Auto add link file vendors to page
+ * ===============================================================
+ */
+gulp.task('inject', () => {
+	let target = gulp.src(path.source + '/partials/*.ejs');
+	let sources = gulp.src([path.vendors + '/*.js', path.vendors + '/*.css'], { read: false });
+	
+	return target.pipe(inject(sources))
+		.pipe(gulp.dest(path.source + '/partials'))
+		.pipe(reload({stream: true}));;
+});
+
+
+
+/**
+ * ===============================================================
+ * Task: Watching task when start gulp
+ * ===============================================================
+ */
 gulp.task('watch', () => {
 	var port = argv.port || 8888;
 
@@ -22,12 +72,18 @@ gulp.task('watch', () => {
 			baseDir: path.output
 		},
 		ui: {
-			port: 8080
+			port: 8080 // port for dashboard of browserSync
 		}
 	});
 
-	gulp.watch([path.source + '/index.html'], ['html']);
+	gulp.watch([path.source + '/**/*.ejs'], ['ejs', 'inject']);
+	gulp.watch(['./build.config.json'], ['copy', 'inject']);
 });
 
-// Task: DEFAULT
-gulp.task('default', ['html', 'watch']);
+
+/**
+ * ===============================================================
+ * Task: DEFAULT
+ * ===============================================================
+ */
+gulp.task('default', ['copy', 'inject', 'ejs', 'watch']);
